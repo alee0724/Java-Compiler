0a6a4ef3d224b20bfd3421fbd390c23bba7a2a83
Annie Lee <112659408+alee0724@users.noreply.github.com>
2024-08-24 16:10:53 -0400

    Transferring updated code to all files pt 2

--- Java Compiler/SymbolTable.java
+++ Java Compiler/SymbolTable.java
@@ -1,55 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class SymbolTable {
-    private List<SymbolTableEntry> entries;
-
-    public SymbolTable() {
-        this.entries = new ArrayList<>();
-    }
+public class SymbolTable {
+    private List<SymbolTableEntry> table = new ArrayList<>();
 
     public void addEntry(String name, String type, int scope, int line) {
-        entries.add(new SymbolTableEntry(name, type, scope, line));
+        table.add(new SymbolTableEntry(name, type, scope, line));
+    }
+
+    public boolean contains(String name, int scope) {
+        return table.stream()
+                    .anyMatch(entry -> entry.getName().equals(name) && entry.getScope() == scope);
+    }
+
+    public SymbolTableEntry getEntry(String name, int scope) {
+        return table.stream()
+                    .filter(entry -> entry.getName().equals(name) && entry.getScope() == scope)
+                    .findFirst()
+                    .orElse(null);
+    }
+
+    public SymbolTableEntry getEntryAcrossScopes(String name) {
+        return table.stream()
+                    .filter(entry -> entry.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
     }
 
     public void printSymbolTable() {
-        System.out.println("Program Symbol Table");
-        System.out.println(" -------------------------------------");
-        for (SymbolTableEntry entry : entries) {
-            System.out.println("[Name: " + entry.getName() + 
-                               ", Type: " + entry.getType() + 
-                               ", Scope: " + entry.getScope() + 
-                               ", Line: " + entry.getLine() + "]");
-        }
-    }
-
-    private class SymbolTableEntry {
-        private String name;
-        private String type;
-        private int scope;
-        private int line;
-
-        public SymbolTableEntry(String name, String type, int scope, int line) {
-            this.name = name;
-            this.type = type;
-            this.scope = scope;
-            this.line = line;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public int getScope() {
-            return scope;
-        }
-
-        public int getLine() {
-            return line;
+        for (SymbolTableEntry entry : table) {
+            System.out.println(entry);
         }
     }
 }
